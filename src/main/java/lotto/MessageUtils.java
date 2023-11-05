a07816efefdac737d630e56a2d95b470b182197b
minw0095 <kmu6898@naver.com>
2023-11-06 03:00:46 +0900

    feat: 입력 메세지

---
+++ src/main/java/lotto/MessageUtils.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class MessageUtils {
+    static final String INPUT_BUY_NUMBER = "구입금액을 입력해 주세요";
+
+    static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요";
+
+    static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요";
+
+
+
+}
